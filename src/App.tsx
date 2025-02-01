a7626d7fc20df76616ec00c70034da8b6edcda96
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-02-02 02:13:34 +0600

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+   <div>
+        <h1 className="text-xl font-bold text-center font-title ">
+      Hello world
+    </h1>
+
+    <Button className="">Click here</Button>
+   </div>
+  )
+}
+
+export default App
